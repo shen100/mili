9709ff8a0b6596bc7ef62a289630d8a37b8d5ad2
wgz <675611281@qq.com>
2017-07-25 00:52:38 +0800

    上传新的请求方法

--- nodejs/client/js/config/index.js
+++ nodejs/client/js/config/index.js
@@ -3,19 +3,64 @@
 const config = {
 	api: {
 		categories: {
-			list: '/api/admin/categories',
-			create: '/api/admin/category/create ',
-			update: '/api/admin/category/update',
-			statusUpdate: '/api/admin/category/status/update'
+			list: {
+				url:'/api/admin/categories',
+				method: 'get',
+				name: 'getCategories',
+				desc: '获取文章分类列表'
+			},
+			create: {
+				url: '/api/admin/category/create',
+				method: 'post',
+				name: 'categoryCreate',
+				desc: '创建文章分类'
+			},
+			update: {
+				url: '/api/admin/category/update',
+				method: 'post',
+				name: 'categoryUpdate',
+				desc: '更新文章分类'
+			},
+			statusUpdate: {
+				url: '/api/admin/category/status/update',
+				method: 'post',
+				name: 'categoryStatus',
+				desc: '更改分类状态'
+			}
 		},
 		articleAdmin: {
-			list: '/api/admin/articles',
-			update: '/api/admin/article/status/update'
+			list: {
+				url: '/api/admin/articles',
+				method: 'get',
+				name: 'getAdminArticles',
+				desc: '管理员身份获取文章列表'
+			},
+			update: {
+				url: '/api/admin/article/status/update',
+				method: 'post',
+				name: 'updateAdminArticles',
+				desc: '更新文章状态'
+			}
 		},
 		article: {
-			create: '/api/article/create',
-			update: '/api/article/update',
-			item: '/api/article/'
+			create: {
+				url: '/api/article/create',
+				method: 'post',
+				name: 'createArticle',
+				desc: '创建文章'
+			},
+			update: {
+				url: '/api/article/update',
+				method: 'post',
+				name: 'updateArticle',
+				desc: '更新文章'
+			},
+			item: {
+				url: '/api/article/:id',
+				method: 'get',
+				name: 'getArticleItem',
+				desc: '获取单个文章信息'
+			}
 		}
 	}
 }
