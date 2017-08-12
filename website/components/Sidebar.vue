89086178c209618c9a98120dd72dc558a720091b
wgz <675611281@qq.com>
2017-08-12 22:39:03 +0800

    修改首页个人信息

--- website/components/Sidebar.vue
+++ website/components/Sidebar.vue
@@ -7,19 +7,22 @@
 						<img class="golang-user-info-icon" src="~assets/images/head.png" alt="">
 					</a>
 					<span class="golang-sidebar-info-box">
-						您好！ {{user.name}}
+						{{user.name}}
 					</span>
 				</div>
-				<div class="golang-user-line">
-					<a href="/change/pwd" class="golang-common-link link-block">修改密码</a>
-					<a href="" class="golang-common-link link-block">忘记密码</a>
+				<div class="golang-user-line user-info-text">
+					积分: 25
 				</div>
+                <div class="golang-user-line user-info-text text-italic">"这家伙很懒，什么个性签名都没有留下"</div>    
 			</template>
             <template v-else>
                 <p>golang123：golang专业中文社区</p>
                 <p>您可以<a class="golang-user-info-action" href="/signin">登录</a>或<a class="golang-user-info-action" href="/signup">注册</a></p>
             </template>
         </div>
+        <div class="golang-user-info" v-if="userStatus">
+            <a href="/topic/create"><button class="signup-button ivu-btn ivu-btn-primary ivu-btn-large">发布话题</button></a>
+        </div>    
         <div class="golang-cell">
 			<div class="title">回复最多的话题</div>
 			<ul>
@@ -62,6 +65,9 @@
         data () {
             return {
             }
+        },
+        mounted () {
+            console.log(this.user)
         }
     }
 </script>
